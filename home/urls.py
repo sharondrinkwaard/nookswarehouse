7c2647099eed62132dc60ddf31a923d6ede8ecf5
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-12-29 21:24:19 +0000

    Add anchor links to another page from base.html

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('home/', views.index, name='index'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
 
 
 def index(request):
