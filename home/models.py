263e3a86ad56d0d766e6a4aecc6a004cf4242688
sharondrinkwaard <sharondrinkwaard@gmail.com>
2023-01-03 16:31:31 +0000

    Add model for contact form on the home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class contact_form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField(default=datetime.now)
+    notes = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'Name: {self.first_name, self.last_name} | Email: {self.email} | Date: {self.date} | Notes: {self.notes}'
