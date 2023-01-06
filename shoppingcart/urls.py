0db36aa48aa7b97eec0c6cfa1a08e0b319b7269c
sharondrinkwaard <sharondrinkwaard@gmail.com>
2023-01-06 13:56:45 +0000

    Add functionality for add to cart view

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('overview_orders/', views.overview_orders, name='overview_orders'),
+    path('add/<article_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def overview_orders(request):
     """ A view to display the orders made by the authenticated customer """
     return render(request, 'shoppingcart/overview_orders.html')
-    
+
+
+def add_to_cart(request, article_id):
+    """ A view to add items to the shopping cart """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    cart = request.session.get('cart', {})
+
+    if article_id in list(cart.keys()):
+        cart[article_id] += quantity
+    else:
+        cart[article_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
