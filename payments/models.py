94488869113f77d95c30e91be611f60987881347
sharondrinkwaard <sharondrinkwaard@gmail.com>
2023-01-14 07:19:09 +0000

    Add model content to payments app

--- payments/models.py
+++ payments/models.py
@@ -1,4 +1,10 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
 
 
 COUNTRIES = (
@@ -23,3 +29,45 @@ class Order(models.Model):
     delivery_costs = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    
+    def _create_order_number(self):
+        """ Creates a unique order number using UUID """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Update grand total each time a line item is added """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        # if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        #     self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        # else:
+        #     self.delivery_cost = 0
+        # self.grand_total = self.order_total + self.delivery_cost
+        # self.save()
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set the order number if there is none """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
