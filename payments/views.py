7d0695d290896a2253743057ff3c67dfdb2d076a
sharondrinkwaard <sharondrinkwaard@gmail.com>
2023-02-17 11:14:43 +0000

    Implement Stripe payments on payments form

--- payments/views.py
+++ payments/views.py
@@ -13,5 +13,7 @@ def payments(request):
     template = 'payments/payments.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51McQpqGL29wYvReMN5NRcKhCFIuOHM7YVGCyeaPx9ylJIKcwerMaSeEw56Ww8aQfTeWobAedsQguQzNQGku1iDGn00X6cduK9s',
+        'client_secret': 'test client secret'
     }
     return render(request, template, context)
