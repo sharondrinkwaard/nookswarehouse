0355b9330698cb377b6f2144d2e42e5ff03b13a9
sharondrinkwaard <sharondrinkwaard@gmail.com>
2023-02-18 22:34:53 +0000

    Add basic stripe checkout functionality

--- payments/views.py
+++ payments/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from shoppingcart.contexts import shopping_content
+import stripe
 
 
 def payments(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'There is nothing in your shoppingcart.')
         return redirect(reverse('products'))
 
+    current_cart = shopping_content(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'payments/payments.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51McQpqGL29wYvReMN5NRcKhCFIuOHM7YVGCyeaPx9ylJIKcwerMaSeEw56Ww8aQfTeWobAedsQguQzNQGku1iDGn00X6cduK9s',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
     return render(request, template, context)
